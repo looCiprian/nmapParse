fdc2c4d6d6f296170fc6615726ef5cadc01bd776
lorenzo grazian <lorenzo.grazian@yarix.com>
2019-01-10 21:19:26 +0100

    Aggiunto parsing degli argomenti

--- nmapParse.py
+++ nmapParse.py
@@ -1,47 +1,106 @@
+import argparse
 import xml.etree.ElementTree as ET
 from prettytable import PrettyTable
+import sys
+import os
+
+def simpleTable(root):
+
+	table = PrettyTable()
+	table.field_names = ["Name", "Target", "ip up", "Start time", "Finish time"]
+
+	target = root.get('args').split(" ")[-1]
+	startTime = root.get('startstr')
+	finshTime = root.find('runstats/finished').get('timestr')
+	ipUp = root.find('runstats/hosts').get('up') 
+
+	table.add_row(["-", target, ipUp, startTime, finshTime])
+
+	print "\n\n\n\n\n"
+	print table
+	print "\n"
 
 
-tree = ET.parse('perParse.xml')
-root = tree.getroot()
-table = PrettyTable()
-tableDetails = PrettyTable()
+def detailedTable(root):
 
-table.field_names = ["Name", "Target", "ip up", "Start time", "Finish time"]
+	tableDetails = PrettyTable()
+	tableDetails.field_names = ["Number", "ip", "protocol/port"]
+	# host label
+	generalCounter=0
 
-tableDetails.field_names = ["Number", "ip", "protocol/port"]
-generalCounter=0
+	# entro in tutti gli host
+	for i in root.findall('host'):	
+		# lista per le porte aperte
+		portList =[]
+		for p in i.findall('ports/port'):
+			# agigungo le porte aperte alla lista
+			portList.append(p.get('portid'))
+
+		# cerco l'ip dell host che sto scansionando
+		for h in i.findall('address'):
+			hostFounded = h.get('addr')
+
+		# cerco lo stato dell'host che sto scansionando
+		for j in i.findall('status'):
+			# se e' up aggiungo una nuova riga alla tabella
+			if j.get('state') == "up":
+				generalCounter +=1
+				tableDetails.add_row([generalCounter, hostFounded, ', '.join(portList)])
+
+	print tableDetails
+
+
+def findFiles(args):
+
+	dirWhereFindFiles = args.file[0]
+
+	foundedFile=[]
+	for file in os.listdir("."):
+	    if file.endswith(".xml"):
+	        foundedFile.append(os.path.join(".", file))
+
+	if len(foundedFile) == 0:
+		print "No file to parse\n\n\n"
+		exit(1)
+	return foundedFile	
+
+
+def parseFile(args):
+
+	foundedFile=[]
+	foundedFile = findFiles(args)
+
+	for file in foundedFile:
+		tree = ET.parse(file)
+		root = tree.getroot()
+
+		if args.verbose:
+			simpleTable(root)
+			detailedTable(root)
+		else:
+			simpleTable(root)
+
+def main():
 
 
 
-target = root.get('args').split(" ")[-1]
-startTime = root.get('startstr')
-finshTime = root.find('runstats/finished').get('timestr')
-ipUp = root.find('runstats/hosts').get('up') 
+	#	detailedTable(root)
 
-table.add_row(["-", target, ipUp, startTime, finshTime])
-
-print table
-print "\n\n\n"
+	parser = argparse.ArgumentParser(description='Process nmap xml for pre-scanning with Nessus.')
+	
+	parser.add_argument("-v","--verbose", help="print detailed table",action="store_true")
+	parser.add_argument("-f", "--file", type=str,help="",nargs=1)
+	args = parser.parse_args()
 
 
-# entro in tutti gli host
-for i in root.findall('host'):	
-	# lista per le porte aperte
-	portList =[]
-	for p in i.findall('ports/port'):
-		# agigungo le porte aperte alla lista
-		portList.append(p.get('portid'))
+	if args.file:
+		parseFile(args)
 
-	# cerco l'ip dell host che sto scansionando
-	for h in i.findall('address'):
-		hostFounded = h.get('addr')
+	else:
+		parser.print_help()
+		sys.exit(1)
 
-	# cerco lo stato dell'host che sto scansionando
-	for j in i.findall('status'):
-		# se e' up aggiungo una nuova riga alla tabella
-		if j.get('state') == "up":
-			generalCounter +=1
-			tableDetails.add_row([generalCounter, hostFounded, ', '.join(portList)])
 
-print tableDetails
+
+if __name__ == '__main__':
+	main()
