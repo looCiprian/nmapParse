5a4ed2678769ee9ae83d990ff9d65bc5a91f0ac5
lorenzo grazian <lorenzograzian@Lorenzos-MacBook-Pro.local>
2019-01-17 19:25:53 +0100

    Added comment and colors for warning,error,ok

--- nmapParse.py
+++ nmapParse.py
@@ -4,6 +4,23 @@ from prettytable import PrettyTable
 import sys
 import os
 
+#@message: messaggio da stampare
+def errorMessage(message):
+	print '\033[91m' + message + '\033[0m'
+
+#@message: messaggio da stampare
+def warningMessage(message):
+	print '\033[93m' + message + '\033[0m'
+
+#@message: messaggio da stampare
+def okMessage(message):
+	print '\033[92m' + message + '\033[0m'
+
+
+# @param: root - radice del file xml
+# @param: outputFile - puntatre al file di output (gia aperto)
+# @param: globalIpUpCounter - conteggio totale di quanti ip sono up
+# @return: numero di ip up trovati nella root
 def simpleTable(root,outputFile,globalIpUpCounter):
 
 	table = PrettyTable()
@@ -18,16 +35,17 @@ def simpleTable(root,outputFile,globalIpUpCounter):
 
 	outputFile.write("")
 	outputFile.write(table.get_string())
-	outputFile.write("\n")
+	outputFile.write("\n\n")
 
 
 	return int(ipUp)
 
-
+# @param: root - radice del file xml
+# @param: outputFile - puntatre al file di output (gia aperto)
 def detailedTable(root,outputFile):
 
 	tableDetails = PrettyTable()
-	tableDetails.field_names = ["Number", "ip", "status:port number:service"]
+	tableDetails.field_names = ["Number", "ip", "status:portNumber:service"]
 	# host label
 	generalCounter=0
 
@@ -70,7 +88,8 @@ def detailedTable(root,outputFile):
 	outputFile.write("\n\n")
 
 
-
+# @param: globalIpUpCounter - numero totale di ip up da scrivere nel file
+# @param: outputFile - puntatre al file di output (gia aperto)
 def totalIpUp(globalIpUpCounter,outputFile):
 	totalTable = PrettyTable()
 	totalTable.field_names = ["Total up hosts founded"]
@@ -80,6 +99,8 @@ def totalIpUp(globalIpUpCounter,outputFile):
 	outputFile.write(totalTable.get_string())
 	outputFile.write("\n")
 
+# @param: args - argomenti passati a stdin
+# @return: lista con i percorsi assoluti dei file per il parser
 def findFiles(args):
 
 	# directory/file da dove cercare i file
@@ -103,20 +124,22 @@ def findFiles(args):
 	return foundedFile
 
 
-
+# @param: args - argomenti passati a stdin
 def parseFile(args):
 
 	foundedFile=[]
 	foundedFile = findFiles(args)
 	globalIpUpCounter = 0
-	outputFile = open("host_information.txt",'a')
+	outputFile = open("host_information.txt",'w')
 
+	# per ogni file eseguo il parsing
 	for file in foundedFile:
 		try:
 			tree = ET.parse(file)
 			root = tree.getroot()
 		except:
-			print "Errore nel parsing del file xml, sei sicuro che sia xml? :)\n"
+			errorMessage("Errore nel parsing del file xml, sei sicuro che sia xml? :)\n")
+			outputFile.close()
 			exit(1)
 
 		# controllo se richiesta la versione verobse (con porte)
@@ -126,19 +149,20 @@ def parseFile(args):
 				globalIpUpCounter += simpleTable(root,outputFile,globalIpUpCounter)
 				detailedTable(root,outputFile)
 			else:
-				print "Verbose output not available with \"-sn\" nmap option"
+				warningMessage("Stai analizzando una scansione di nmap con opzione \"-sn\", il file verra' analizzato con l'opzione scansione non dettagliata...\n")
+				globalIpUpCounter += simpleTable(root,outputFile,globalIpUpCounter)
 		else:
 			globalIpUpCounter += simpleTable(root,outputFile,globalIpUpCounter)
 
+	# scriviamo il numero di ip totali trovati
 	totalIpUp(globalIpUpCounter,outputFile)
+	okMessage("Sono stati analizzati " + str(globalIpUpCounter) + " aprire il file \"host_information.txt\" per maggiori dettagli \nLo trovi nella cartella del parser" )
 
+	# chiudiamo il file
 	outputFile.close()
 
+
 def main():
-
-
-
-	#	detailedTable(root)
 
 	parser = argparse.ArgumentParser(description='Process nmap xml for pre-scanning with Nessus.')
 
